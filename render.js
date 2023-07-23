775b764efce170e632140e89858bf0fb5f7f0f4f
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-07-23 11:20:53 +0700

    16 не работает кнопка добавления коммента

--- render.js
+++ render.js
@@ -1,4 +1,4 @@
-import { getComments } from "./app.js";
+/*/import { getComments } from "./app.js";
 import { initEventListeners } from "./main.js";
 import { comments } from "./main.js";
 
@@ -141,4 +141,76 @@ buttonElement.addEventListener("click", () => {
 
 
 })
+}/*/
+
+import { initEventListeners } from "./main.js";
+import { comments } from "./main.js";
+import { renderСomments } from "./main.js";
+export const renderPage = () => {
+  const pageElement = document.getElementById("container");
+  const commentsHtml = comments
+    .map(
+      (comment, index) => `
+      <li class="comment" data-likeNumb="${comment.likes}" data-comment-text="${comment.text} (${comment.author.name})">
+        <div class="comment-header">
+          <div>${comment.author.name}</div>
+          <div>${comment.date}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+            ${comment.text}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${comment.likes}</span>
+            <button class="like-button"></button>
+          </div>
+        </div>
+        <div>
+          <button data-index="${index}" class="delete-button">Удалить комментарий</button>
+        </div>
+      </li>`
+    )
+    .join("");
+    
+  const addHtml = `
+    <div id="add-form" class="add-form">
+      <input
+        id="add-form-name"
+        type="text"
+        class="add-form-name"
+        placeholder="Введите ваше имя"
+      />
+      <textarea
+        id="add-form-text"
+        type="textarea"
+        class="add-form-text"
+        placeholder="Введите ваш коментарий"
+        rows="4"
+      ></textarea>
+      <div class="add-form-row">
+        <button id="add-form-button" class="add-form-button">Написать</button>
+      </div>
+    </div>`;
+
+  pageElement.innerHTML = commentsHtml + addHtml;
+
+  const deleteButtonsElements = document.querySelectorAll(".delete-button");
+
+  for (const deleteButtonElement of deleteButtonsElements) {
+    deleteButtonElement.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const index = deleteButtonElement.dataset.index;
+      comments.splice(index, 1);
+      renderСomments();
+      renderPage();
+    });
+  }
+  
+  initEventListeners();
+};
+
+export function userComment() {
+  loader.textContent = "";
 }
